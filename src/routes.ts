167040479ce394ad1a6f09d235d0ce8d98082b2b
bryandmoreira <bryan.d.moreira@aluno.senai.br>
2026-04-16 17:17:48 -0300

    Add files via upload

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express"
-import { deflate } from "node:zlib"
+
+import alunoController from "./controllers/alunos"
+import cursoController from "./controllers/cursos"
 
 const routes = Router()
 
@@ -7,4 +9,22 @@ routes.get("/", (request, response) =>
      response.status(200).json("Servidor rodando"),
 )
 
+routes.post("/alunos", alunoController.create)
+routes.get("/alunos", alunoController.list)
+routes.get("/alunos/:id", alunoController.getById)
+routes.put("/alunos/:id", alunoController.update)
+routes.delete("/alunos/:id", alunoController.delete)
+
+
+routes.post("cursos", cursoController.create)
+routes.get("cursos", cursoController.list)
+routes.get("/cursos/:id", cursoController.getById)
+routes.put("/cursos/:id", cursoController.update)
+routes.delete("/cursos/:id", cursoController.delete)
+
+
+
+
+
 export default routes
+
